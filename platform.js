480ce31b9fb32d60035de6f4d0cbfb0d0b470e77
D33-cyber <dylan.lee@nicolet.us>
2026-05-22 11:09:23 -0500

    Add files via upload

--- platform.js
+++ platform.js
@@ -5,34 +5,15 @@ const heel = document.querySelector('.object--1');
 const restartButton = document.querySelector('.restart');
 
 let playerY = 300;
-let velocity = 0;
-let gravity = 0;
-let jumping = false;
-
 let heelX = 1100;
 let heelSpeed = 8;
 
-document.addEventListener("keydown", (event) => {
-    if (event.code === "Space" && !jumping) {
-        velocity = -16;
-        jumping = true;
-    }
-});
+
 
 function gameLoop() {
 
-  
-
-    // FLOOR
-    if (playerY > 250) {
-        playerY = 250;
-        velocity = 0;
-        jumping = false;
-    }
 
     player.style.top = `${playerY}px`;
-
-    // MOVE HEEL
     heelX -= heelSpeed;
 
    
